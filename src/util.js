1387ca9a9b4364fd7328369354849301e51a3ea6
fran pérez <francis@tid.es>
2017-04-24 22:31:47 +0200

    move Utils and UrlManager to utils

---
+++ src/util.js
@@ -0,0 +1,102 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const md5 = require("md5");
+var Utils;
+(function (Utils) {
+    function isFunction(f) {
+        return typeof f === 'function';
+    }
+    Utils.isFunction = isFunction;
+    function isObject(o) {
+        return typeof o === 'object';
+    }
+    Utils.isObject = isObject;
+    function funcify(f) {
+        return this.isFunction(f) ? f : function () { return f; };
+    }
+    Utils.funcify = funcify;
+    function isArray(a) {
+        return Array.isArray(a);
+    }
+    Utils.isArray = isArray;
+    function arrify(a) {
+        return this.isArray(a) ? a : a ? [a] : [];
+    }
+    Utils.arrify = arrify;
+    function getKeys(o) {
+        var keys = [];
+        for (var key in o)
+            keys.push(key);
+        return keys;
+    }
+    Utils.getKeys = getKeys;
+    function extend(obj) {
+        Array.prototype.slice.call(arguments, 1).forEach(function (source) {
+            for (var prop in source) {
+                try {
+                    //recursively merge object properties
+                    if (source[prop].constructor == Object) {
+                        obj[prop] = this.extend(obj[prop], source[prop]);
+                    }
+                    else {
+                        if (source[prop] !== void 0)
+                            obj[prop] = source[prop];
+                    }
+                }
+                catch (e) {
+                    // Property in destination object not set; create it and set its value.
+                    obj[prop] = source[prop];
+                }
+            }
+        });
+    }
+    Utils.extend = extend;
+    function md5HashFunction(item) {
+        return md5(JSON.stringify(item));
+    }
+    Utils.md5HashFunction = md5HashFunction;
+    function idHashFunction(item) {
+        return ('id' in item) ? item.id : md5HashFunction(item);
+    }
+    Utils.idHashFunction = idHashFunction;
+    function reflect(promise) {
+        return promise.then(function (v) {
+            return { data: v, status: "resolved" };
+        }, function (e) { return { data: e, status: "rejected" }; });
+    }
+    Utils.reflect = reflect;
+    function flatten(object) {
+        return Object.assign({}, ...function _flatten(objectBit, path = '') {
+            return [].concat(//concat everything into one level
+            ...Object.keys(objectBit).map(//iterate over object
+            key => typeof objectBit[key] === 'object' && objectBit[key] !== null ?
+                _flatten(objectBit[key], `${path.length ? path + "." : path}${key}`) :
+                ({ [`${path.length ? path + "." : path}${key}`]: objectBit[key] }) //append object with it’s path as key
+            ));
+        }(object));
+    }
+    Utils.flatten = flatten;
+    ;
+})(Utils = exports.Utils || (exports.Utils = {}));
+class UrlManager {
+    /**
+     *
+     */
+    constructor(config) {
+        this.visitedUrls = [];
+        this.config = config;
+    }
+    tryAddUrl(url) {
+        var validUrl = this.config.newHashNewPage ? url.split('#')[0] : url;
+        var isValidUrl = (!(this.config.allowRepeatedUrls && url in this.visitedUrls));
+        if (isValidUrl)
+            this.visitedUrls.push(url);
+        return isValidUrl;
+    }
+    addUrls(urls) {
+        var _self = this;
+        return urls.filter(function (u) { return _self.tryAddUrl(u); });
+    }
+}
+exports.UrlManager = UrlManager;
+//# sourceMappingURL=util.js.map
---
+++ src/util.ts
@@ -0,0 +1,100 @@
+import md5 = require("md5");
+import { Settings } from "./settings";
+
+export module Utils {
+    export function isFunction (f) : boolean {
+        return typeof f === 'function';
+    }
+
+    export function isObject (o) : boolean{
+        return typeof o === 'object';
+    }
+
+    export function funcify(f) {
+        return this.isFunction(f) ? f : function() { return f; };
+    }
+
+    export function isArray(a) : boolean {
+        return Array.isArray(a);
+    }
+
+    export function arrify<T>(a) : T[] {
+        return this.isArray(a) ? a : a ? [a] : [];
+    }
+
+    export function getKeys(o) : string[] {
+        var keys = [];
+        for (var key in o) keys.push(key);
+        return keys;
+    }
+
+    export function extend(obj) : any {
+        Array.prototype.slice.call(arguments, 1).forEach(function(source) {
+            for (var prop in source) {
+                try {
+                    //recursively merge object properties
+                    if ( source[prop].constructor==Object ) {
+                        obj[prop] =this.extend(obj[prop], source[prop]);
+                    } else {
+                        if (source[prop] !== void 0) obj[prop] = source[prop];
+                    }
+                } catch(e) {
+                    // Property in destination object not set; create it and set its value.
+                    obj[prop] = source[prop];
+                }
+            }
+        });
+    }
+
+    export function md5HashFunction (item: any){
+        return md5(JSON.stringify(item));
+    }
+
+    export function idHashFunction (item: any) {
+        return ('id' in item) ? item.id : md5HashFunction(item);
+    }
+
+    export function reflect(promise){
+        return promise.then(function(v){ 
+            return {data:v, status: "resolved" }
+        },
+                            function(e){ return {data:e, status: "rejected" }});
+    }
+
+    export function flatten (object) {
+        return Object.assign( {}, ...function _flatten( objectBit, path = '' ) {  //spread the result into our return object
+            return [].concat(                                                       //concat everything into one level
+            ...Object.keys( objectBit ).map(                                      //iterate over object
+                key => typeof objectBit[ key ] === 'object' && objectBit [ key ] !== null ?                       //check if there is a nested object
+                _flatten( objectBit[ key ], `${ path.length ? path + "." : path }${ key }` ) :              //call itself if there is
+                ( { [ `${ path.length ? path + "." : path }${ key }` ]: objectBit[ key ] } )                //append object with it’s path as key
+            )
+            )
+        }( object ) );
+    };
+}
+
+export class UrlManager {
+    visitedUrls : string[] = [];
+    config: Settings;
+
+    /**
+     *
+     */
+    constructor(config: Settings) {
+        this.config = config;
+    }
+
+    tryAddUrl (url: string) : boolean{
+        var validUrl = this.config.newHashNewPage ? url.split('#')[0] : url;
+        var isValidUrl = (!(this.config.allowRepeatedUrls && url in this.visitedUrls))
+        if (isValidUrl)
+            this.visitedUrls.push(url);
+        return isValidUrl;
+    }
+
+    addUrls (urls: string[]) : string[]{
+        var _self = this;
+        return urls.filter(function(u) { return _self.tryAddUrl(u); });
+    }
+}
