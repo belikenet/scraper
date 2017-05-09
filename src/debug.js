9fa93b1c7c043a86ba11e88684e7e6cb0b2f760d
fran pérez <francis@tid.es>
2017-05-09 13:18:47 +0200

    output files from typescript compilation go to dist folder remove *.js and *.js.map files from source control

--- src/debug.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const scrape_1 = require("./scrape");
-var scraper = new scrape_1.Scraper();
-scraper.init();
-//# sourceMappingURL=debug.js.map
